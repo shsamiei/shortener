527485d0805984e6f9d5610edc67996de887b8bc
hossein <shsamiei27ap@gmail.com>
2022-11-17 01:44:29 +0330

    int2base added for convert 10 base to another base

--- LinkShortener/services.py
+++ LinkShortener/services.py
@@ -1,4 +1,29 @@
 class ShortenerService:
 
-    def test(self, url):
-        return 'google.com'
+
+
+
+
+
+    @classmethod
+    def _int2base(cls, x, base=64):
+        if x < 0:
+            sign = -1
+        elif x == 0:
+            return cls.digs[0]
+        else:
+            sign = 1
+
+        x *= sign
+        digits = []
+
+        while x:
+            digits.append(cls.digs[int(x % base)])
+            x = int(x / base)
+
+        if sign < 0:
+            digits.append('-')
+
+        digits.reverse()
+
+        return ''.join(digits)
