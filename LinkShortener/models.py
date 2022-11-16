ae54b63ba88adc616ab8ae3c807737a289f8dfac
hossein <shsamiei27ap@gmail.com>
2022-11-16 21:31:36 +0330

    Shortener model added

--- LinkShortener/models.py
+++ LinkShortener/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Shortener(models.Model):
+    url = models.CharField(max_length=255, db_index=True)
+    shortener = models.CharField(max_length=255, db_index=True)
+
