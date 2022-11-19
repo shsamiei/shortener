b751dc51e4cd17c16cae7c8b5c65a9e1c91ecd78
hossein <shsamiei27ap@gmail.com>
2022-11-19 14:17:57 +0330

    everythings work great

--- LinkShortener/models.py
+++ LinkShortener/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Shortener(models.Model):
-    url = models.CharField(max_length=255, db_index=True)
+    url = models.CharField(max_length=255, db_index=True, unique=True)
     shortener = models.CharField(max_length=255, db_index=True)
     clicks = models.IntegerField(default=0)
 
